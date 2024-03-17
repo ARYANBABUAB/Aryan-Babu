86894b4377f6175947aa2a84bd612fb599a8fe4c
ARYANBABUAB <157523788+ARYANBABUAB@users.noreply.github.com>
2024-03-17 17:34:07 +0500

    Update config.js

--- config.js
+++ config.js
@@ -66,7 +66,7 @@ global.fglog = 'STAR.jpg'
 global.thumb = fs.readFileSync('./STAR.jpg')
 
 
-global.wait = '*🕣 _Amir IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
+global.wait = '*🕣 _A Babu IS LOADING..._*\n*▰▰▰▱▱▱▱▱⭐*'
 global.rwait = '🔜'
 global.dmoji = '⚡'
 global.done = '☑️'
